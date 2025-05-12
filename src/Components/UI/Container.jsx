bf00571a951bf701699ed7b68b4be97e4a6c20fb
Asif Mahmud <asifmzx@gmail.com>
2025-05-12 17:29:22 +0600

    Initial Setup

---
+++ src/Components/UI/Container.jsx
@@ -0,0 +1,7 @@
+const Container = ({ children, className = "" }) => {
+    return (
+        <div className={`w-full max-w-[1920px] mx-auto  ${className}`}>
+            {children}
+        </div>
+    );
+};
