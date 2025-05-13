947f2589f344c69908dd70f6b5e8dbda807e3cb3
Asif Mahmud <asifmzx@gmail.com>
2025-05-13 12:27:39 +0600

    NavBar Done

--- src/Components/Pages/HeroSection.jsx
+++ src/Components/Pages/HeroSection.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Button from "../UI/Button";
 
 const HeroSection = () => {
-  return (
-    <div>
-      <Button variant="secondary"/>
-    </div>
-  );
+    return (
+        <div>
+            <Button variant="secondary" text="Checking" />
+        </div>
+    );
 };
 
 export default HeroSection;
