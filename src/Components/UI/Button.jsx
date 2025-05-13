da6206b73849ba64b712ffce8f4d0eef1aed5610
MD Rakibul Islam <rakibul15-12212@diu.edu.bd>
2025-05-13 10:27:38 +0600

    add loading,not found and button component

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+const Button = ({ variant = "primary", text = "button" }) => {
+  const baseStyles = "px-2 py-2 rounded text-xs font-medium";
+  const variants = {
+    primary: "bg-[#FFFFFF] hover:bg-[#F5F5F5] text-black",
+    secondary: "bg-[#0A0A0A] hover:bg-[#262626] text-[#FAFAFA] ",
+  };
 
-const Button = () => {
   return (
-    <div>
-      <button className="px-5 px 3 bg-black text-white">Click it</button>
-    </div>
+    <button className={`${baseStyles} ${variants[variant]}`}>
+      <div className="flex items-center space-x-1">{text}</div>
+    </button>
   );
 };
 
--- src/Components/UI/Container.jsx
+++ src/Components/UI/Container.jsx
@@ -1,4 +1,4 @@
-const Container = ({ children, className = "" }) => {
+export const Container = ({ children, className = "" }) => {
     return (
         <div className={`w-full max-w-[1920px] mx-auto  ${className}`}>
             {children}
