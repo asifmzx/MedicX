23ae0cdffa3c6dc6499c87299da195b69da1405f
MD Rakibul Islam <rakibul15-12212@diu.edu.bd>
2025-06-01 11:15:24 +0600

    setting's integrate section updated

--- src/Components/Pages/Settings/Integrations/Active/Active.jsx
+++ src/Components/Pages/Settings/Integrations/Active/Active.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { IntegrationData, statusColors } from "../IntegrationsData";
+import { Switch } from "antd";
+import Button from "@/Components/UI/Button/Button";
 
 const Active = () => {
-  return <div>Active</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:p-4  ">
+      {IntegrationData.active.map((item, index) => (
+        <div
+          key={index}
+          className="bg-white p-4 border border-gray-300 rounded-lg"
+        >
+          <div>
+            <div className="flex items-center justify-between mb-4">
+              <p className="text-2xl font-semibold">{item.title}</p>
+              <p>{statusColors[item.status]}</p>
+            </div>
+            <p className="mt-3 text-gray-500">{item.description}</p>
+            <div className="flex items-center justify-between mt-10">
+              <div className="flex items-center space-x-3">
+                <div className="bg-[#E7E7E7] p-3 rounded-full inline-block text-center ">
+                  {item.icon}
+                </div>
+                <div className="flex flex-col">
+                  <span className="font-semibold">{item.text1}</span>
+                  <span className="text-sm text-gray-500">{item.text2}</span>
+                </div>
+              </div>
+              <div>
+                <Switch />
+              </div>
+            </div>
+          </div>
+          <div className="mt-4">
+            <Button
+              variant="primary"
+              text="Configure"
+              className="w-full border border-gray-300"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Active;
--- src/Components/Pages/Settings/Integrations/IntegrationsData.js
+++ src/Components/Pages/Settings/Integrations/IntegrationsData.js
@@ -1,7 +1,14 @@
+import Status from "@/Components/UI/Status/Status";
 import { FiMessageSquare } from "react-icons/fi";
 import { GoCreditCard } from "react-icons/go";
+import { IoVideocamOutline } from "react-icons/io5";
+import { LuCalendarCheck } from "react-icons/lu";
+import { PiScan, PiTicket } from "react-icons/pi";
 
-const { PiScan } = require("react-icons/pi");
+export const statusColors = {
+  Connected: <Status type="settings" variant="Connected" text="Connected" />,
+  Available: <Status type="settings" variant="Available" text="Available" />,
+};
 
 export const IntegrationData = {
   active: [
@@ -10,7 +17,7 @@ export const IntegrationData = {
       description: "Sync patient records with your EHR system",
       text1: "MediSync EHR",
       text2: "Last synced: 2 hours ago",
-      icon: <PiScan />,
+      icon: <PiScan size={30} />,
       status: "Connected",
     },
     {
@@ -18,7 +25,7 @@ export const IntegrationData = {
       description: "Process payments and manage transactions",
       text1: "MediPay",
       text2: "Connected account: ****4589",
-      icon: <GoCreditCard />,
+      icon: <GoCreditCard size={30} />,
       status: "Connected",
     },
     {
@@ -26,7 +33,7 @@ export const IntegrationData = {
       description: "Send appointment reminders via SMS",
       text1: "TextAlert",
       text2: "Credits: 2,450 remaining",
-      icon: <FiMessageSquare />,
+      icon: <FiMessageSquare size={30} />,
       status: "Connected",
     },
   ],
@@ -36,7 +43,7 @@ export const IntegrationData = {
       description: "Conduct virtual appointments with patients",
       text1: "MediConnect",
       text2: "HIPAA compliant video calls",
-      icon: <PiScan />,
+      icon: <IoVideocamOutline size={30} />,
       status: "Available",
     },
 
@@ -45,7 +52,7 @@ export const IntegrationData = {
       description: "Verify patient insurance eligibility",
       text1: "InsureCheck",
       text2: "Real-time eligibility verification",
-      icon: <FiMessageSquare />,
+      icon: <PiTicket size={30} />,
       status: "Available",
     },
     {
@@ -53,7 +60,7 @@ export const IntegrationData = {
       description: "Integrate with laboratory systems",
       text1: "LabConnect",
       text2: "Automated lab results import",
-      icon: <GoCreditCard />,
+      icon: <LuCalendarCheck size={30} />,
       status: "Available",
     },
   ],
