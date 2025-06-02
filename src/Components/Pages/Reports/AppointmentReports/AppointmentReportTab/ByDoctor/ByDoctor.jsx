852cf309dd72933a631e8b689b2cdb8939541f26
MD Rakibul Islam <rakibul15-12212@diu.edu.bd>
2025-06-02 13:51:22 +0600

    Reports/RecentAppointment/ByService Section Completed

--- src/Components/Pages/Reports/AppointmentReports/AppointmentReportTab/ByDoctor/ByDoctor.jsx
+++ src/Components/Pages/Reports/AppointmentReports/AppointmentReportTab/ByDoctor/ByDoctor.jsx
@@ -5,7 +5,7 @@ import DoctorPerformance from "./DoctorPerformance/DoctorPerformance";
 const ByDoctor = () => {
   return (
     <div>
-      {" "}
+     
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
         <div className="col-span-1 sm:col-span-2">
           <AppointmentsByDoctor />
