020e909ed0de9766b9c07c6d1610a85b495edb81
MD Rakibul Islam <rakibul15-12212@diu.edu.bd>
2025-06-27 16:48:11 +0600

    sidebar issue fix (#69)
    * admin route fix
    
    * admin route fix
    
    * add blur effect on sidebar

--- src/app/home/Admin/layout.jsx
+++ src/app/home/Admin/layout.jsx
@@ -10,28 +10,38 @@ export default function HomeLayout({ children }) {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
       {/* Fixed Header */}
-      <div className="fixed top-0 left-0 right-0 z-30 h-16">
+      <div className="fixed top-0 left-0 right-0 z-50 h-16">
         <Header toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
       </div>
 
       {/* Main Body */}
-      <div className="flex flex-1 pt-16">
+      <div className="flex flex-1 pt-16 relative">
         {/* Sidebar */}
         <aside
-          className={`ScrollBar fixed top-16 left-0 h-[calc(100vh-4rem)] bg-white z-20 overflow-y-auto transform transition-transform duration-200 ease-in-out
-          w-100% md:translate-x-0 md:static md:block
-          ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
+          className={`
+            ScrollBar fixed top-16 left-0 h-[calc(100vh-4rem)] bg-white z-40 overflow-y-auto transform transition-transform duration-300 ease-in-out
+            w-100%  md:translate-x-0 md:static md:block
+            ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}
+          `}
         >
           <AdminSidebar onMenuItemClick={() => setSidebarOpen(false)} />
         </aside>
 
+        {/* Backdrop overlay - only visible on mobile when sidebar open */}
+        {sidebarOpen && (
+          <div
+            className="fixed inset-0 bg-transparent backdrop-blur-xs z-30 md:hidden"
+            onClick={() => setSidebarOpen(false)}
+          ></div>
+        )}
+
         {/* Main Content Area */}
         <main
-          className={`
+          className="
             flex-1 overflow-y-auto bg-gray-50 h-[calc(100vh-4rem)] ScrollBar
             transition-all duration-300 ease-in-out
-            
-          `}
+           
+          "
         >
           {children}
         </main>
